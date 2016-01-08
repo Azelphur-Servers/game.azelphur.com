c0266797f869052a50fcd03764e6ade057fdc833
Alfie "Azelphur" Day <support@azelphur.com>
2016-01-08 20:22:40 +0000

    Add DONATION_AMOUNTS to local_settings.py.example

---
+++ game/local_settings.py
@@ -0,0 +1,68 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+# If this file is changed in development, the development server will
+# have to be manually restarted because changes will not be noticed
+# immediately.
+
+DEBUG = True
+PAYPAL_TEST = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "Lorem Ipsum"
+NEVERCACHE_KEY = "Lorem Ipsum"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+# Social Auth API keys
+SOCIAL_AUTH_STEAM_API_KEY = "5BA6EE88DEE4C387C030D0DD9C56B4BA"
+
+PAYPAL_RECEIVER_EMAIL = "azelphur-facilitator@azelphur.com"
+
+DONATION_AMOUNTS = (
+    # Amount, Days of premium
+    (2, 7),
+    (5, 30),
+    (10, 90),
+    (17, 180),
+)
+
+PREMIUM_GROUP_NAME = "SM_PREMIUM"
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+# Domains for public site
+# ALLOWED_HOSTS = [""]
+
+# These settings are used by the default fabfile.py provided.
+# Check fabfile.py for defaults.
+
+# FABRIC = {
+#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
+#     "SSH_USER": "",  # VPS SSH username
+#     "HOSTS": [""],  # The IP address of your VPS
+#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
+#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
+#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+#     "DB_PASS": "",  # Live database password
+#     "ADMIN_PASS": "",  # Live admin user password
+#     "SECRET_KEY": SECRET_KEY,
+#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+# }
