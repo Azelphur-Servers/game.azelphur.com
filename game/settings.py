a8273249367f8dd7a233329a9e44ee387efb5999
Alfie "Azelphur" Day <support@azelphur.com>
2016-01-08 22:49:49 +0000

    Add game_info context processor for sidebar

--- game/settings.py
+++ game/settings.py
@@ -251,6 +251,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "social.apps.django_app.context_processors.backends",
     "social.apps.django_app.context_processors.login_redirect",
     "donations.processors.donations",
+    "game_info.processors.servers",
 )
 
 # List of middleware classes to use. Order is important; in the request phase,
