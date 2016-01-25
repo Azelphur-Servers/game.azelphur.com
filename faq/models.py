947a37752153e7e1d7a38226ffe107b98b51d4e7
Alfie "Azelphur" Day <support@azelphur.com>
2016-01-25 22:48:18 +0000

    FAQs now support markdown.

--- faq/models.py
+++ faq/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from markdown import markdown
 
 
 class FAQ(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    body_html = models.TextField(
+        editable=False,
+        blank=True,
+        null=True
+    )
 
     def __unicode__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.body_html = markdown(self.body)
+        super(FAQ, self).save(*args, **kwargs)
