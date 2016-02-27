e2711018025b27c94d57d344fc27b0a0d24f89f4
Alfie "Azelphur" Day <azelphur@azelphur.com>
2016-02-27 16:41:15 +0000

    Donations now support being added from the admin panel

--- donations/models.py
+++ donations/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from django.conf import settings
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
@@ -16,17 +18,31 @@ class PremiumDonation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     end_time = models.DateTimeField()
 
-
-def reload_admins():
-    servers = Server.objects.all()
-    for server in servers:
-        with RCON((server.host, server.port), settings.RCON_PASSWORD) as rcon:
-            print(rcon("sm_reloadadmins"))
+    def __unicode__(self):
+        return "%s expires at %s" % (self.user, self.end_time)
 
 
+@receiver(post_delete, sender=PremiumDonation)
+def premium_post_delete(sender, instance, **kwargs):
+    group = Group.objects.get(name=settings.PREMIUM_GROUP_NAME)
+    instance.user.groups.remove(group)
+    profile = Profile.objects.get(user=instance.user)
+    profile.status = ""
+    profile.save()
+
+@receiver(post_save, sender=PremiumDonation)
+def premium_post_save(sender, instance, created, **kwargs):
+    if created:
+        group = Group.objects.get(name=settings.PREMIUM_GROUP_NAME)
+        instance.user.groups.add(group)
+        profile = Profile.objects.get(user=instance.user)
+        profile.status = "Premium"
+        profile.save()
+
+@receiver(valid_ipn_received)
 def add_premium(sender, **kwargs):
     """
-        When PayPal IPN completes, add user to the PREMIUM group and
+        When PayPal IPN completes, add the users premium
         set an expiry time
     """
     ipn_obj = sender
@@ -36,11 +52,6 @@ def add_premium(sender, **kwargs):
         except ObjectDoesNotExist:
             # Todo: do something here as something has gone wrong
             return
-        group = Group.objects.get(name=settings.PREMIUM_GROUP_NAME)
-        social_user.user.groups.add(group)
-        profile = Profile.objects.get(user=social_user.user)
-        profile.status = "Premium"
-        profile.save()
         for x in settings.DONATION_AMOUNTS:
             if x[0] == ipn_obj.mc_gross:
                 end_time = timezone.now() + timedelta(days=x[1])
@@ -49,4 +60,9 @@ def add_premium(sender, **kwargs):
                     end_time=end_time
                 ).save()
 
-valid_ipn_received.connect(add_premium)
+
+def reload_admins():
+    servers = Server.objects.all()
+    for server in servers:
+        with RCON((server.host, server.port), settings.RCON_PASSWORD) as rcon:
+            print(rcon("sm_reloadadmins"))
