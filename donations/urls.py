f1d758ebe42dbff06fc42f30c7a5f0f33e4b707f
Alfie "Azelphur" Day <support@azelphur.com>
2016-03-01 19:11:52 +0000

    Ingame donate page support

--- donations/urls.py
+++ donations/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from .views import DonateView
 
 urlpatterns = [
-    url(r'^', DonateView.as_view(), name='donate'),
+    url(r'^$', DonateView.as_view(), name='donate'),
+    url(r'^ig/$', DonateView.as_view(template_name='donations/donate_ig.html'), name='donate-ig'),
 ]
